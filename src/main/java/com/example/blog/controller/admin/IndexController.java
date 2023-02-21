47f0f072646e916bc8522e332f5af62b83dd1d5e
JingxuanWei <Jn728702@dal.ca>
2023-02-20 23:08:04 -0400

    index controller

--- src/main/java/com/example/blog/controller/admin/IndexController.java
+++ src/main/java/com/example/blog/controller/admin/IndexController.java
@@ -1,33 +1,20 @@
 package com.example.blog.controller.admin;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.core.toolkit.Wrappers;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.example.blog.dto.JsonResult;
-import com.example.blog.entity.Category;
-import com.example.blog.service.CategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * (Category)table controller
- *
- * @author makejava
- * @since 2022-01-08 13:30:00
+ * @author JingxuanWei
+ * Template: esay code
  */
 @Controller("adminIndexController")
 @RequestMapping("/admin")
 public class IndexController {
-
     @GetMapping
     public String index(Model model) {
         model.addAttribute("tab", "index");
         return "admin/index";
     }
-
 }
