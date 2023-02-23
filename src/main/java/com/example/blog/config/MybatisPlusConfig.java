e0ef868f22bd57c57905118f6df09160abaa1f6d
JingxuanWei <Jn728702@dal.ca>
2023-02-23 00:57:14 -0400

    MybatisPlusConfig.java

--- src/main/java/com/example/blog/config/MybatisPlusConfig.java
+++ src/main/java/com/example/blog/config/MybatisPlusConfig.java
@@ -5,37 +5,16 @@ import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
-/**
- * @author 言曌
- * @date 2018/12/22 下午1:49
- */
-
 @Configuration
 public class MybatisPlusConfig {
-
-    /**
-     * MyBatis Plus分页
-     * @return PaginationInterceptor 分页插件
-     */
     @Bean
     public PaginationInterceptor paginationInterceptor() {
         return new PaginationInterceptor();
     }
-
-    /***
-     * plus 的性能优化
-     * @return
-     */
     @Bean
     public PerformanceInterceptor performanceInterceptor() {
-        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
-        /*<!-- SQL 执行性能分析，开发环境使用，线上不推荐。 maxTime 指的是 sql 最大执行时长 -->*/
-        performanceInterceptor.setMaxTime(1000);
-        /*<!--SQL是否格式化 默认false-->*/
-        performanceInterceptor.setFormat(false);
-        return performanceInterceptor;
+        return new PerformanceInterceptor()
+                .setMaxTime(1000)
+                .setFormat(false);
     }
-
-
 }
