b164de09dd3cc23cd8674c43d33e4aee3f99babc
JingxuanWei <Jn728702@dal.ca>
2023-02-23 00:35:39 -0400

    update config里面内容

--- src/main/java/com/example/blog/config/MvcConfig.java
+++ src/main/java/com/example/blog/config/MvcConfig.java
@@ -1,43 +1,27 @@
 package com.example.blog.config;
-
 import com.example.blog.interceptor.UserInfoInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
-/**
- * 拦截器，资源路径配置
- */
+
 @Slf4j
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
-
-
-    /**
-     * 配置静态资源路径
-     *
-     * @param registry registry
-     */
     @Override
+    /**
+     * 静态资源映射用于访问静态资源, 比如图片, js, css等储存好的文件和文件夹
+     */
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/");
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/templates/themes/")
-                .addResourceLocations("classpath:/robots.txt");
-        registry.addResourceHandler("/upload/**")
-                .addResourceLocations("file:///" + System.getProperties().getProperty("user.home") + "/sens/upload/");
-        registry.addResourceHandler("/favicon.ico")
-                .addResourceLocations("classpath:/static/images/favicon.ico");
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/templates/themes/", "classpath:/robots.txt");
+        registry.addResourceHandler("/upload/**").addResourceLocations("file:~/sens/upload/");
+        registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/static/images/favicon.ico");
     }
-
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        InterceptorRegistration registration = registry.addInterceptor(new UserInfoInterceptor());
-        registration.addPathPatterns("/admin");
-        registration.addPathPatterns("/admin/**");
-        registration.excludePathPatterns("/admin/login");
-        registration.excludePathPatterns("/admin/logout");
+        InterceptorRegistration registration = registry.addInterceptor(new UserInfoInterceptor())
+                .addPathPatterns("/admin", "/admin/**")
+                .excludePathPatterns("/admin/login", "/admin/logout");
     }
-
 }
--- src/main/java/com/example/blog/config/MybatisPlusConfig.java
+++ src/main/java/com/example/blog/config/MybatisPlusConfig.java
@@ -5,36 +5,29 @@ import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 /**
- * @author 言曌
- * @date 2018/12/22 下午1:49
+ * MyBatis Plus配置类
+ * @author JingxuanWei
  */
-
 @Configuration
 public class MybatisPlusConfig {
 
-    /***
-     * plus 的性能优化
-     * @return
-     */
-    @Bean
-    public PerformanceInterceptor performanceInterceptor() {
-        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
-        /*<!-- SQL 执行性能分析，开发环境使用，线上不推荐。 maxTime 指的是 sql 最大执行时长 -->*/
-        performanceInterceptor.setMaxTime(1000);
-        /*<!--SQL是否格式化 默认false-->*/
-        performanceInterceptor.setFormat(false);
-        return performanceInterceptor;
-    }
-
     /**
-     * mybatis-plus分页插件
+     * MyBatis Plus分页
+     * @return PaginationInterceptor 分页插件
      */
     @Bean
     public PaginationInterceptor paginationInterceptor() {
         return new PaginationInterceptor();
     }
 
-
+    /**
+     * 配置MyBatis Plus性能分析插件, 设置最长查询时间和是否格式化SQL语句
+     */
+    @Bean
+    public PerformanceInterceptor performanceInterceptor() {
+        return new PerformanceInterceptor()
+                .setMaxTime(1000)
+                .setFormat(false);
+    }
 }
