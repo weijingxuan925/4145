62ab2d533eb66d04e080e984811a834ba76a5554
JingxuanWei <Jn728702@dal.ca>
2023-02-23 20:01:46 -0400

    Data Transfer Object 的 json 更新

--- src/main/java/com/example/blog/dto/JsonResult.java
+++ src/main/java/com/example/blog/dto/JsonResult.java
@@ -1,12 +1,13 @@
 package com.example.blog.dto;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
- *
  * @author : JingxuanWei
- * @date : 2023/2/17
+ * @since  : 2023/2/17
  */
 @Data
+@NoArgsConstructor
 public class JsonResult {
     private Integer code;
     private String msg;
@@ -14,39 +15,46 @@ public class JsonResult {
 
     /**
      * @param code is the code
-     * @param message  is the information to be returned
+     * @param msg  is the msg need to return
      */
-    public JsonResult(Integer code, String message) {
+    public JsonResult(Integer code, String msg) {
         this.code = code;
-        this.msg = message;
+        this.msg = msg;
     }
 
-    public JsonResult(Integer code, String message, Object result) {
+    /**
+     * @param code is the code need to return
+     * @param msg is the message need to return
+     * @param result is the result need to return
+     */
+    public JsonResult(Integer code, String msg, Object result) {
         this.code = code;
-        this.msg = message;
+        this.msg = msg;
         this.result = result;
     }
+
     /**
      * @param code is the code
      * @param result is the result
+     * @deprecated 无使用场景
      */
     public JsonResult(Integer code, Object result) {
         this.code = code;
         this.result = result;
     }
-    public static JsonResult error(String msg) {
-        return new JsonResult(0, msg);
+    public static JsonResult success(String message) {
+        return new JsonResult(1, message);
     }
-    public static JsonResult error(String msg, Object data) {
-        return new JsonResult(0, msg, data);
+    public static JsonResult success(String message, Object result) {
+        return new JsonResult(1, message, result);
+    }
+    public static JsonResult error(String message) {
+        return new JsonResult(0, message);
     }
     public static JsonResult success() {
-        return new JsonResult(1, "Success!!");
+        return new JsonResult(1, "Successfully!");
     }
-    public static JsonResult success(String msg) {
-        return new JsonResult(1, msg);
-    }
-    public static JsonResult success(String msg, Object result) {
-        return new JsonResult(1, msg, result);
+    public static JsonResult error(String message, Object data) {
+        return new JsonResult(0, message, data);
     }
 }
