5dd826ba4709873b3ed8f62fcc5493afb4c41e2c
JingxuanWei <Jn728702@dal.ca>
2023-02-23 20:17:14 -0400

    UserInfoInterceptor.java

--- src/main/java/com/example/blog/interceptor/UserInfoInterceptor.java
+++ src/main/java/com/example/blog/interceptor/UserInfoInterceptor.java
@@ -1,30 +1,32 @@
 package com.example.blog.interceptor;
-
 import com.example.blog.entity.User;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.HandlerInterceptor;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * @author 言曌
- * @date 2021/12/29 11:36 下午
+ * @author JingxuanWei
+ * @since 2023/2/23
  */
 @Service
 public class UserInfoInterceptor implements HandlerInterceptor {
 
 
-
+    /**
+     * 如果用户已经登录，那么就放行，否则就跳转到登录页面
+     * @param request 请求
+     * @param response 响应
+     * @param o 处理器
+     */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) throws IOException {
         User user = (User) request.getSession().getAttribute("user");
-        if (user == null) {
-            response.sendRedirect("/admin/login");
-            return false;
+        if (user != null) {
+            return true;
         }
-        return true;
+        response.sendRedirect("/admin/login");
+        return false;
     }
-
 }
