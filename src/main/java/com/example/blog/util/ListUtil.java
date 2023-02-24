41440df6d8892494524bef159f3459fa7fbd4f02
JingxuanWei <Jn728702@dal.ca>
2023-02-23 20:29:57 -0400

    list util

--- src/main/java/com/example/blog/util/ListUtil.java
+++ src/main/java/com/example/blog/util/ListUtil.java
@@ -1,22 +1,19 @@
 package com.example.blog.util;
-
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
- * @author 言曌
- * @date 2022/1/8 7:23 下午
+ * @author JingxuanWei
+ * @since 2023/2/23
  */
 
 public class ListUtil {
 
     public static String list2Str(List<String> list) {
-        StringBuilder sb = new StringBuilder();
-        for (String str : list) {
-            sb = sb.append(str).append(",");
+        if (list == null || list.isEmpty()) {
+            return "";
         }
-        if (sb.length() > 0) {
-            return sb.substring(0, sb.length() - 1);
-        }
-        return sb.toString();
+        return list.stream().map(Object::toString).collect(Collectors.joining(","));
     }
+
 }
